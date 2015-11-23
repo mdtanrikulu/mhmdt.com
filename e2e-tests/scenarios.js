ea6266ca0e9c27a7844277628b116df939b79cd2
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2015-11-23 05:56:49 +0100

    test files updated

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -5,37 +5,37 @@
 describe('my app', function() {
 
 
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
+  it('should automatically redirect to /me when location hash/fragment is empty', function() {
     browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
+    expect(browser.getLocationAbsUrl()).toMatch("/me");
   });
 
 
   describe('view1', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html#/me');
     });
 
 
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    it('should render me when user navigates to /me', function() {
+      expect(element.all(by.css('[ng-view] #name')).first().getText()).
+        toMatch(/Muhammed Tanrıkulu/);
     });
 
   });
 
 
-  describe('view2', function() {
+  describe('details', function() {
 
     beforeEach(function() {
-      browser.get('index.html#/view2');
+      browser.get('index.html#/details');
     });
 
 
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
+    it('should render details when user navigates to /details', function() {
+      expect(element.all(by.css('[ng-view] #name_d')).first().getText()).
+        toMatch(/Muhammed Tanrıkulu/);
     });
 
   });
